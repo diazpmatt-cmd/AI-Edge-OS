d80d3c26b37b8242b81bcd445f3e296ebe82b528
diazpmatt-cmd <diaz.p.matt@gmail.com>
2026-07-08 16:08:20 -0500

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttpImport from "pino-http";
 import path from "path";
 import { fileURLToPath } from "url";
 

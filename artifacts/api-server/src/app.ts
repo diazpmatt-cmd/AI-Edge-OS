7dc53c207bb570abafe8e029f2baee45468f56d9
diazpmatt-cmd <diaz.p.matt@gmail.com>
2026-07-08 17:06:22 -0500

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -23,14 +23,14 @@ const app: Express = express();
 app.use(
   pinoHttp({
     logger,
-    serializers: {
-      req(req: any) {
-  return { id: req.id, method: req.method, url: req.url?.split("?")[0] };
+   serializers: {
+  req(req: any) {
+    return { id: req.id, method: req.method, url: req.url?.split("?")[0] };
+  },
+  res(res: any) {
+    return { statusCode: res.statusCode };
+  },
 },
-res(res: any) {
-  return { statusCode: res.statusCode };
-},
-    },
   }),
 );
 

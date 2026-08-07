ff3cb3b47cf5b366307e1ab6ebbb9d8a5779fade
diazpmatt-cmd <diaz.p.matt@gmail.com>
2026-08-07 00:01:48 -0500

    Export Authority outreach draft schema

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -25,6 +25,7 @@ export * from "./service-registry";
 export * from "./discovery";
 export * from "./discovery-schedules";
 export * from "./backlinks";
+export * from "./authority-outreach-drafts";
 export * from "./authority-profiles";
 export * from "./platform-deliveries";
 export * from "./agent-tasks";

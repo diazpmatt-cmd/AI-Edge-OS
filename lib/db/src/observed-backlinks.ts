045623cf1edc0058494cf8482ee5ca27e594a61e
diazpmatt-cmd <diaz.p.matt@gmail.com>
2026-08-14 09:18:22 -0500

    feat: export backlink inventory provider boundary

--- lib/db/src/observed-backlinks.ts
+++ lib/db/src/observed-backlinks.ts
@@ -1,3 +1,5 @@
 export * from "./observed-backlink-lifecycle";
 export * from "./observed-backlink-repository";
+export * from "./backlink-inventory-provider";
+export * from "./dataforseo-backlink-inventory-adapter";
 export * from "./schema/observed-backlinks";
